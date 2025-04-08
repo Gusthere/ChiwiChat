f2973b7aa655213bf8853c8f73fca9b5ad8fd98c
Gustavo Heredia <171433163+Gusthere@users.noreply.github.com>
2025-04-07 20:08:51 -0400

    Corrección de la zona horaria del sistema

--- usuarios_service/public/index.php
+++ usuarios_service/public/index.php
@@ -13,6 +13,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+date_default_timezone_set("America/Caracas");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
